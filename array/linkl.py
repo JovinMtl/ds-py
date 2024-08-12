2f96fa9e4fd453d3cfeff2824aa10d1843ec032a
JovinMtl <nsanzumukizath@gmail.com>
2024-08-12 14:56:17 +0200

    defining the LinkedList and Node class.

---
+++ array/linkl.py
@@ -0,0 +1,17 @@
+
+class Node:
+    """This class represents the node of the LinkedList"""
+    def __init__(self) -> None:
+        data = '' #anything
+        next = None # will hold that next object(node)
+
+class LinkedList:
+    """This class is for linkedList."""
+    def __init__(self) -> None:
+        first = None  # will hold first object
+        last = None
+        size = 0
+
+
+# initializing the LinkedList
+lklst = LinkedList()
