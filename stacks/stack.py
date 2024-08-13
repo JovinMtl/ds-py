96bf8f8ada4c020acdecf3b5f98d8d19fb411f0b
JovinMtl <nsanzumukizath@gmail.com>
2024-08-13 13:27:21 +0200

    Initialized the stack.

--- stacks/stack.py
+++ stacks/stack.py
@@ -1,5 +1,12 @@
 
 
+from ..LnkLst import Node
 
 class Stack:
     """This class is for implementing the stack behaviour."""
+    def __init__(self) -> None:
+        self.latest = None // the latest state of the Stack
+        self.previous = None // the previous Node of the stack.
+
+        return None
+    
