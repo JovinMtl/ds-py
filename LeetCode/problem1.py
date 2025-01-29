60129dd8adbae2c66e91b3d2d837a06241b85f16
JovinMtl <nsanzumukizath@gmail.com>
2025-01-29 12:01:34 +0200

    gathering all the solutions to pick the longest one

--- LeetCode/problem1.py
+++ LeetCode/problem1.py
@@ -7,25 +7,36 @@ class ProbOne:
     def solution(self):
         l_d = len(self.data)
         i = 1
+        solutions = []
         if self.data == self._re(self.data):
-            return self.data
+            solutions.append(self.data)
+            # return self.data
         while i < (l_d-1):
             m1 = self.data[i:] # taking the first caracter to match the rest
             if self._re(m1) == m1:
-                return m1
+                solutions.append(m1)
+                # return m1
             j = 1
             while j <= (l_d-1): # the rest to match with, or m1 to be the next
                 m2 = self.data[:-j]
                 m3 = self.data[i:-j]
                 if self._re(m2) == m2 and \
                     len(m2)>1:
-                    return m2
+                    solutions.append(m2)
+                    # return m2
                 elif self._re(m3) == m3 and \
                     len(m3)>1:
-                    return m3
+                    solutions.append(m3)
+                    # return m3
                 j += 1
             i += 1
-        return self.data[0]
+        solutions.append(self.data[0])
+        longest = ''
+        print(f"We have {len(solutions)} solutions")
+        for solution in solutions:
+            if len(solution) > len(longest):
+                longest = solution
+        return longest
     
     def _re(self, data):
         # This method is for reversing the string
