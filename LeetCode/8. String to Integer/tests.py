0bce7230408f6e4f2e4522e99f9a4728eeccd18f
JovinMtl <nsanzumukizath@gmail.com>
2025-02-03 19:13:28 +0200

    test_one okay

---
+++ LeetCode/8. String to Integer/tests.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+from StringtoInteger import StringtoInteger
+
+
+class TestStringtoInteger(unittest.TestCase):
+    def setUp(self):
+        self.obj = StringtoInteger()
+    
+    def test_one(self):
+        s = "1"
+        self.assertEqual(self.obj.convert(s), 0)
