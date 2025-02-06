7de7bc1f034959a9e3ff96c545bc4cd732d46488
JovinMtl <nsanzumukizath@gmail.com>
2025-02-06 14:26:00 +0200

    test_zigzag_three

--- LeetCode/6. Zigzag Conversion/ZigzagConversion.py
+++ LeetCode/6. Zigzag Conversion/ZigzagConversion.py
@@ -1,5 +1,25 @@
 
  
 class ZigzagConversion:
-    def convert(self, data:str)->str:
-        return data
+    def convert(self, data:str, numRows:int=1)->str:
+        arr = []
+        i = 0
+        t = 0
+        print(f"With len of {len(data)}: {data}")
+        while i < numRows:
+            if t == len(data):
+                break
+            arr.append([])
+            print(f"Consider: i{i}, index:{t}, value:{data[t]}")
+            arr[i].append(data[t])
+            t += 1
+            if i == (numRows - 1):
+                j = numRows - 2
+                while j >= 0:
+                    arr[j].append(data[t])
+                    j -= 1
+                    t += 1
+            i += 1
+            if (i == numRows) and (t < len(data)):
+                i = 1
+        return arr
--- LeetCode/6. Zigzag Conversion/tests.py
+++ LeetCode/6. Zigzag Conversion/tests.py
@@ -8,7 +8,17 @@ class ZigzagConversionTest(unittest.TestCase):
     def setUp(self):
         self.obj = ZigzagConversion()
     
-    def test_zigzag_one(self):
-        data = "jove"
-        result = self.obj.convert(data=data)
-        self.assertEqual(result, "jove")
+    # def test_zigzag_one(self):
+    #     data = "PAYPALISHIRING"
+    #     result = self.obj.convert(data=data, numRows=3)
+    #     self.assertEqual(result, "PINALSIGYAHRPI")
+    
+    def test_zigzag_two(self):
+        data = "ABCDE"
+        result = self.obj.convert(data=data, numRows=3)
+        self.assertEqual(result, "AEBDC")
+    
+    def test_zigzag_three(self):
+        data = "ABCDEF"
+        result = self.obj.convert(data=data, numRows=3)
+        self.assertEqual(result, "AEBDFC")
