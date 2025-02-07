2d7f4f9391f4a68877218b8e7c9cf9ba4fb09ea0
JovinMtl <nsanzumukizath@gmail.com>
2025-02-07 22:22:12 +0200

    implemented toRoman method

--- LeetCode/12. IntegertoRoman/integertoRoman.py
+++ LeetCode/12. IntegertoRoman/integertoRoman.py
@@ -2,4 +2,33 @@
 
 class IntegertoRoman:
     def toRoman(self, data:int)->str:
-        return "MM"
+        number = data
+        roman = ""
+        milles = int(number / 1000)
+        if milles:
+            number -= (milles * 1000)
+            roman += "M"*milles
+        cinq_cent = int(number / 500)
+        if cinq_cent:
+            number -= (cinq_cent * 500)
+            roman += "D"*cinq_cent
+        cents = int(number / 100)
+        if cents:
+            number -= (cents * 100)
+            roman += "C"*100
+        cinquante = int(number / 50)
+        if cinquante:
+            number -= (cinquante * 50)
+            roman += "L"*cinquante
+        dix = int(number / 10)
+        if dix:
+            number -= (dix * 10)
+            roman += "X"*dix
+        cinq = int(number / 5)
+        if cinq:
+            number -= (cinq * 5)
+            roman += "V"*cinq
+        if number:
+            roman += "I"*number
+        return roman
+    
