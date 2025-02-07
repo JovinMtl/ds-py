e531cd87d216397bef570933577f9f8698d60db5
JovinMtl <nsanzumukizath@gmail.com>
2025-02-07 21:58:29 +0200

    init palindromeNumber

---
+++ LeetCode/9. PalindromeNumber/palindromeNumber.py
@@ -0,0 +1,5 @@
+
+
+class PalindromeNumber:
+    def check(self, data:int)->bool:
+        return False
---
+++ LeetCode/9. PalindromeNumber/tests.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+from palindromeNumber import PalindromeNumber
+
+
+class Solution(unittest.TestCase):
+    def setUp(self):
+        self.obj = PalindromeNumber()
+    
+    def test_palindromeNumber_one(self):
+        number = 120
+        result = self.obj.check(data=number)
+        self.assertEqual(result, False)
