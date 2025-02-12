c352f61e9db1ce6d3bca0d3f6b27aad49bea4bf7
JovinMtl <nsanzumukizath@gmail.com>
2025-02-12 12:39:26 +0200

    added integertoRoman

--- LeetCode/12. IntegertoRoman/integertoRoman.py
+++ LeetCode/12. IntegertoRoman/integertoRoman.py
@@ -13,17 +13,23 @@ class IntegertoRoman:
             number -= (cinq_cent * 500)
             roman += "D"*cinq_cent
         cents = int(number / 100)
+        print(f"Number after cinq_cent:{number}")
         if cents:
             number -= (cents * 100)
-            roman += "C"*100
+            roman += "C"*cents
+        print(f"Number after cents: {number}")
         cinquante = int(number / 50)
         if cinquante:
             number -= (cinquante * 50)
             roman += "L"*cinquante
-        dix = int(number / 10)
-        if dix:
+        if number > 10 and number < 50:
+            dix = int(number / 10)
+            roman += "X"*(5-dix)
+            roman += "L"
             number -= (dix * 10)
-            roman += "X"*dix
+        if number == 9:
+            number -= 9
+            roman += "IX"
         cinq = int(number / 5)
         if cinq:
             number -= (cinq * 5)
--- LeetCode/12. IntegertoRoman/tests.py
+++ LeetCode/12. IntegertoRoman/tests.py
@@ -12,3 +12,8 @@ class Solution(unittest.TestCase):
         number = 2000
         result = self.obj.toRoman(data=number)
         self.assertEqual(result, "MM")
+    
+    def test_integertoRoman_two(self):
+        number = 3749
+        result = self.obj.toRoman(data=number)
+        self.assertEqual(result, "MMMDCCXLIX")
