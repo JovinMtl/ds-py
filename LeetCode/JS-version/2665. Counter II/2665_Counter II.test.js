7eb6afe98898f4a8a52978d0047c4eec05bef726
JovinMtl <nsanzumukizath@gmail.com>
2025-02-28 13:08:44 +0200

    testing the reset method

--- LeetCode/JS-version/2665. Counter II/2665_Counter II.test.js
+++ LeetCode/JS-version/2665. Counter II/2665_Counter II.test.js
@@ -3,6 +3,10 @@ let createCounter = require('./2665_Counter II')
 
 
 const counter = createCounter(5)
+
 test("See increment", ()=>{
-    expect(counter.increment()).toBe(6)
+    expect(counter.increment()).toBe(6);
+    expect(counter.decrement()).toBe(5);
+    expect(counter.decrement()).toBe(4);
+    expect(counter.reset()).toBe(5);
 });
