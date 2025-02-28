f9cc620bdbfc7d95985138646d03b908a8fbad91
JovinMtl <nsanzumukizath@gmail.com>
2025-02-28 12:20:06 +0200

    exporting createCounter function

--- LeetCode/JS-version/2665. Counter II/2665_Counter II.js
+++ LeetCode/JS-version/2665. Counter II/2665_Counter II.js
@@ -26,8 +26,9 @@ var createCounter = function(init) {
   * counter.reset(); // 5
   * counter.decrement(); // 4
   */
-  const counter = createCounter(5)
-  // counter.increment(); // 6
-  console.log(counter.increment())
-  console.log(counter.reset())
-  console.log(counter.decrement())
+//   const counter = createCounter(5)
+//   console.log(counter.increment())
+//   console.log(counter.reset())
+//   console.log(counter.decrement())
+
+module.exports = createCounter;
