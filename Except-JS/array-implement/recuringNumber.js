4b2cc9cdfe03877f51fe676c9f2c8dafeaf6e4ce
JovinMtl <nsanzumukizath@gmail.com>
2025-03-28 15:55:07 +0200

    defined recuringNumber function

---
+++ Except-JS/array-implement/recuringNumber.js
@@ -0,0 +1,16 @@
+
+// given an array [2, 1, 1, 3, 5]  => 1
+
+function recuringNumber(arr){
+    let cumilated = {}
+    let result = undefined
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] in cumilated){
+            result = arr[i]
+            break
+        }
+        cumilated[arr[i]] = 0
+    }
+    return result
+}
+console.log("The result is : ", recuringNumber([2, 5, 1, 2, 3, 5, 1, 2, 4]))
