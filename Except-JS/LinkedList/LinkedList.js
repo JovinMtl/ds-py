30b25f1d2e3b9bece0145b2dbb52894407132ff3
JovinMtl <nsanzumukizath@gmail.com>
2025-04-02 10:41:17 +0200

    managed empty append

--- Except-JS/LinkedList/LinkedList.js
+++ Except-JS/LinkedList/LinkedList.js
@@ -10,14 +10,19 @@ class LinkedList {
         this.length = firstNode ? 1 : 0
     }
     append(nodeValue){
-        this.tail.next = {
-            val: nodeValue,
+        let last = {
+            val: nodeValue ? nodeValue : null,
             next: null
         }
+        this.tail.next = last;
+        this.tail = last;
+        this.length++;
     }
 }
 
 const myLinkedList = new LinkedList(5)
 myLinkedList.append(2)
+myLinkedList.append()
+myLinkedList.append(3)
 
 console.log("THe Lklst: " + JSON.stringify(myLinkedList))
