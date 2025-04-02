36fb33e9bfbdc56268edc17bd308733d6a5ce2f2
JovinMtl <nsanzumukizath@gmail.com>
2025-04-02 11:01:41 +0200

    defined remove and pop methods to the LinkedList

--- Except-JS/LinkedList/LinkedList.js
+++ Except-JS/LinkedList/LinkedList.js
@@ -26,6 +26,28 @@ class LinkedList {
         this.head = first
         this.length++
     }
+    remove(index){
+        if (Number(index) >= this.length){
+            return undefined
+        }
+        if (Number(index) == 0){
+            this.pop()
+            return
+        }
+        let current = this.head
+        for (let i = 0; i <= index; i++){
+            if (i == (index -1)){
+                // let tempNode = current;
+                current.next = current.next.next;
+                this.length--;
+            }
+            current = current.next;
+        }
+    }
+    pop(){
+        this.head = this.head?.next;
+        this.length--;
+    }
 }
 
 const myLinkedList = new LinkedList(5)
@@ -33,5 +55,8 @@ myLinkedList.append(2)
 myLinkedList.append(8)
 myLinkedList.append(3)
 myLinkedList.prepend(12)
+// myLinkedList.remove(2)
+myLinkedList.remove(0)
+myLinkedList.pop()
 
 console.log("THe Lklst: " + JSON.stringify(myLinkedList))
