43306a1197a4ef0a30679217565a15273105bc89
JovinMtl <nsanzumukizath@gmail.com>
2025-04-02 10:44:21 +0200

    added prepend method to the linkedlist

--- Except-JS/LinkedList/LinkedList.js
+++ Except-JS/LinkedList/LinkedList.js
@@ -18,11 +18,20 @@ class LinkedList {
         this.tail = last;
         this.length++;
     }
+    prepend(nodeValue){
+        let first = {
+            val: nodeValue ? nodeValue : null,
+            next: this.head
+        }
+        this.head = first
+        this.length++
+    }
 }
 
 const myLinkedList = new LinkedList(5)
 myLinkedList.append(2)
-myLinkedList.append()
+myLinkedList.append(8)
 myLinkedList.append(3)
+myLinkedList.prepend(12)
 
 console.log("THe Lklst: " + JSON.stringify(myLinkedList))
